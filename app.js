cf080651cb0fb6e4757e7def83a6c53cfe8bf41a
Davido1214 <60898488+Davido1214@users.noreply.github.com>
2024-04-24 02:34:57 -0700

    added and completed reviews section and began photos section/services section-not sure which it will be yet

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const menuBackdropParent = document.querySelector("#menuBackdropParent");
 const heroImage = document.querySelector("#heroImage")
 const slideOverMenu = document.querySelector("#slideOverMenu");
 const images = ["./assets/haircut_img_1.jpeg","./assets/haircut_img_2.jpeg","./assets/haircut_img_3.jpeg","./assets/haircut_img_4.jpeg"]
+const bookmark = document.querySelector('.bookmark')
 
 buttons.forEach(function(button){
     button.addEventListener('click', function(){
@@ -16,6 +17,11 @@ buttons.forEach(function(button){
     });
 });
 
+bookmark.addEventListener('click',function(){
+    menuBackdrop.classList.toggle('hidden');
+    menuBackdropParent.classList.toggle('hidden');
+    slideOverMenu.classList.toggle('hidden');
+})
 
 window.addEventListener('load', function(){
     
